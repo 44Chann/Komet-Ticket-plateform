56ac07a10a072e41bc68cb717cc29998d82bab1d
= <=>
2022-05-28 17:43:06 +0530

    collection details page added

--- pages/CollectionDetails.tsx
+++ pages/CollectionDetails.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { useRouter } from "next/router";
 
 const CollectionDetails = () => {
-    const [collection, setCollection] = useState<any>({})
+    const [collection, setCollection] = useState<any>()
     const { authRedirect, isConnected, baseurl } = useAppContext()
     const router = useRouter()
     const fetchCollection = async () => {
@@ -18,37 +18,32 @@ const CollectionDetails = () => {
     }
 
     useEffect(() => {
-        authRedirect()
         fetchCollection()
-    }, [])
+    })
 
 
     return (
         <> {
-            isConnected ? (
-                <div className=" flex w-full  flex-col lg:flex-row items-center min-h-[80vh] py-9">
-                    <div className="lg:w-[40%]">
-                        <img className="object-cover" src={collection.collectionImage} alt="" />
+
+            <div className=" flex w-full  flex-col lg:flex-row items-center min-h-[80vh] py-9">
+                <div className="lg:w-[40%]">
+                    <img className="object-cover" src={collection ? collection.collectionImage : ""} alt="" />
+                </div>
+                <div className=" w-full lg:w-[60%] min-h-[400px] mb-5 lg:p-5 p-3 my-6">
+                    <div className="py-5 border-b border-purple-400">
+                        <p>Created By : {collection ? collection.collectionName : ""}</p>
                     </div>
-                    <div className=" w-full lg:w-[60%] min-h-[400px] mb-5 lg:p-5 p-3 my-6">
-                        <div className="flex justify-between">
-                            <h1></h1>
-                            <Btn text="share" onclick={() => { }} />
-                        </div>
-                        <div className="py-5 border-b border-purple-400">
-                            <p>Created By : {collection.collectionName}</p>
-                        </div>
-                        <div className="py-3">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa architecto quas neque consequuntur! Odit impedit optio unde ab eaque possimus doloribus necessitatibus, cumque, eveniet magnam totam quibusdam laboriosam? Saepe explicabo in minus ex! Aspernatur mollitia reiciendis, porro at aut a amet hic illum dolorum eos sequi harum, placeat exercitationem quod.</p>
-                        </div>
-                        <div className="flex flex-col lg:flex-row mt-9 ">
-                            <DetailsBadge title="price" value={collection.collectionPrice + " Matic"} />
-                            <DetailsBadge title="tickets" value={collection.numberOfTokensOnSale} />
-                            <DetailsBadge title="Adress" value={collection.collectionContractId.slice(0, 5)} />
-                        </div>
+                    <div className="py-3">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa architecto quas neque consequuntur! Odit impedit optio unde ab eaque possimus doloribus necessitatibus, cumque, eveniet magnam totam quibusdam laboriosam? Saepe explicabo in minus ex! Aspernatur mollitia reiciendis, porro at aut a amet hic illum dolorum eos sequi harum, placeat exercitationem quod.</p>
+                    </div>
+                    <div className="flex flex-col lg:flex-row mt-9 ">
+                        <DetailsBadge title="price" value={collection ? collection.collectionPrice + " Matic" : ""} />
+                        <DetailsBadge title="tickets" value={collection ? collection.numberOfTokensOnSale : ""} />
+                        <DetailsBadge title="Adress" value={collection ? collection.collectionContractId.slice(0, 5) : ""} />
                     </div>
                 </div>
-            ) : null
+            </div>
+
         }
 
         </>
